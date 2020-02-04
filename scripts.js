3110fca27f2ce4fc4d8fa7a059d9f119803b0dd9
darena-mafhaque <51241870+darena-mafhaque@users.noreply.github.com>
2020-02-04 17:03:52 -0500

    Removed unnecessary event handler

--- scripts.js
+++ scripts.js
@@ -1,9 +1,3 @@
-window.onload = () => {
-  window.addEventListener('resize', () => {
-    if (window.innerWidth > 630) document.querySelector('.mobile-nav').style.display = 'none';
-  })
-}
-
 let toggleMenu = () => {
   let nav = document.querySelector('.mobile-nav');
   if (nav.style.display === 'flex')
