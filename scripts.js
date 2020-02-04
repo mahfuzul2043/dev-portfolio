239beca8fab734715913663904ad18864b5b9940
darena-mafhaque <51241870+darena-mafhaque@users.noreply.github.com>
2020-02-04 01:24:42 -0500

    When clicking on anything other than the mobile nav while it's expanded, then it will close

--- scripts.js
+++ scripts.js
@@ -11,3 +11,11 @@ let toggleMenu = () => {
   else
     nav.style.display = 'flex';
 };
+
+window.onclick = e => {
+  if (e.srcElement !== document.querySelector('.menu-icon')) {
+    let nav = document.querySelector('.mobile-nav');
+    if (nav.style.display === 'flex')
+      nav.style.display = 'none';
+  }
+}
